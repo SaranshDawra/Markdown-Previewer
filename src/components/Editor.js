1aca61d1bfa0a59753f9a775cd56dfab30d7b6c6
SaranshDawra <saransh2000dawra@gmail.com>
2020-10-19 21:12:01 +0530

    Editor ui updated

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,10 +3,7 @@ import style from './editor.module.css';
 
 export default function Editor(props) {
     return (
-        <div className={style.Editor}>
-            <h1 className={style.titleBar}>Editor</h1>
-            <textarea className={style.editor} value={props.value} onChange={(e) => props.changed(e.target.value)}>
+            <textarea className={style.Editor} value={props.value} onChange={(e) => props.changed(e.target.value)}>
             </textarea>
-        </div>
     )
 }
