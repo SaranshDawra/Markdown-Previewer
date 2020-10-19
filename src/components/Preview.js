6121335a744d198d6b4878fb70f721524dbe95fc
SaranshDawra <saransh2000dawra@gmail.com>
2020-10-19 21:13:00 +0530

    Previewer ui updated

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import style from './preview.module.css';
-import marked from  'marked';
+import React from "react";
+import style from "./preview.module.css";
+import marked from "marked";
 
 export default function Preview(props) {
     return (
-        <div className={style.Preview}>
-            <h1 className={style.titleBar}>Previewer</h1>
-            <div className={style.previewer} dangerouslySetInnerHTML={{__html: marked(props.value)}}></div>
-        </div>
-    )
+        <div
+            className={style.Previewer}
+            dangerouslySetInnerHTML={{ __html: marked(props.value) }}
+        ></div>
+    );
 }
