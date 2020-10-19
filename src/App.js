d412440415e47f173335d588cb8b73f95374b933
SaranshDawra <saransh2000dawra@gmail.com>
2020-10-19 21:14:17 +0530

    apps ui updated

--- src/App.js
+++ src/App.js
@@ -1,45 +1,70 @@
 import React from "react";
 import Editor from "./components/Editor";
-import Preview from './components/Preview';
-import Footer from './components/Footer';
-import "./App.css";
+import Preview from "./components/Preview";
+import Footer from "./components/Footer";
+import classes from "./App.module.css";
 
 class App extends React.Component {
+    initialState = `# Welcome to MD-Previewer
+MD-Previewer is an online markdown editor built by [Saransh Dawra](https://github.com/SaranshDawra)
 
-  initialState =`
-  This is a paragraph
+## 📄 List
 
-  # Heading
-  ## Heading 2
+### Number List
+1. One
+2. Two
+3. Three
 
-  - list item 1
-  - list item 2
-  - list item 3
+### Bulletpoint List
+* One
+* Two
+* Three
+      
+### Dash also works!
+- One
+- Two
+- Three
 
-  [Visit my GitHub](https://github.com/SaranshDawra)
+### Images
+<img src="https://media.giphy.com/media/l3HBbltOYjoNq/giphy.gif" height="200px"></img>
+      
+# 📝 Typography
+      
+### Headings
+      
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+      
+### Quotes
+> Lorem ipsum dolor sit amet, consectetuer adipiscing elit.Donec odio.Quisque volutpat mattis eros.
 `;
 
-  state = {
-    value : this.initialState
-  }
+    state = {
+        value: this.initialState,
+    };
 
-  updateChangeHandler = (value) => {
-    this.setState({value});
-  }
+    updateChangeHandler = (value) => {
+        this.setState({ value });
+    };
 
-  render() {
-    return(
-      <div className="container text-center">
-          <h1 className="title">Markdown Previewer</h1>
-          <Editor value={this.state.value} changed={this.updateChangeHandler}></Editor>
-          <br/>
-          <br/>
-          <Preview value={this.state.value}></Preview>
-          <br/>
-          <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={classes.Container}>
+                <div className={classes.Title}>MD Previewer</div>
+                <div className={classes.InnerCont}>
+                    <Editor
+                        value={this.state.value}
+                        changed={this.updateChangeHandler}
+                    ></Editor>
+                    <Preview value={this.state.value}></Preview>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
